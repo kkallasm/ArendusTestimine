878b02ec56d3641361fbb4e759697ac5c8264643
siimav <siim.aaver@gmail.com>
2011-10-16 19:53:25 +0300

    BowlingCalc tests

--- Arendustestimine/Test/kodune_yl/BowlingCalcTests.java
+++ Arendustestimine/Test/kodune_yl/BowlingCalcTests.java
@@ -1,5 +1,62 @@
 package kodune_yl;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BowlingCalcTests {
 
+	@Test
+	public void Test1() {
+		BowlingCalc calc = new BowlingCalc();
+	}
+	
+	@Test
+	public void Test2() {
+		BowlingCalc calc = new BowlingCalc();
+		for (int i = 0; i < 20; i++) { // 20 viset iga kord üks maha
+			calc.hit(1);
+		}
+		assertEquals(20, calc.getScore());
+	}
+	
+	@Test
+	public void Test3() {
+		BowlingCalc calc = new BowlingCalc();
+		for (int i = 0; i < 20; i++) {
+			calc.hit(2);
+		}
+		assertEquals(40, calc.getScore());
+	}
+	
+	@Test
+	public void Test4() {
+		BowlingCalc calc = new BowlingCalc();
+		for (int i = 0; i < 21; i++) {
+			calc.hit(5);
+		}
+		assertEquals(150, calc.getScore());
+	}
+	
+	@Test
+	public void Test5() {
+		BowlingCalc calc = new BowlingCalc();
+		for (int i = 0; i < 21; i++) {
+			calc.hit(10);
+		}
+		//TODO
+	}
+	
+	@Test
+	public void Test6() {
+		BowlingCalc calc = new BowlingCalc();
+		calc.hit(4);
+		calc.hit(5);
+		calc.hit(8);
+		calc.hit(2);
+		calc.hit(10);
+		
+		//TODO: ...
+	}
+	
 }
--- Arendustestimine/src/kodune_yl/BowlingCalc.java
+++ Arendustestimine/src/kodune_yl/BowlingCalc.java
@@ -5,5 +5,15 @@ public class BowlingCalc {
 	public BowlingCalc(){
 		System.out.println("sadasd");
 	}
+
+	public void hit(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getScore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
